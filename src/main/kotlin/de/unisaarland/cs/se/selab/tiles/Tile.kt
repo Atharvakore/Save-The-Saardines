936f094ff5d9e84d73a744177eb3b2e1675901be
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-09-13 14:09:17 +0200

    make neighbours internal

--- src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Tile.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Tile.kt
@@ -3,7 +3,7 @@ package de.unisaarland.cs.se.selab.tiles
 class Tile() {
     private var id: Int = 0
     private var pos: Vec2D = Vec2D(0, 0)
-    private var neighbours: Array<Tile?> = Array(6) { null }
+    internal var neighbours: Array<Tile?> = Array(6) { null }
     // # of restrictions acting on this tile. if > 0, then tile is not traversable.
     private var restrictions: Int = 0
 }
