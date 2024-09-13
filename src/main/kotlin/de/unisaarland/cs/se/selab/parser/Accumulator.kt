0672259a373883ea3eda6eaf00a9ac92cbccd814
Fedayn <badr.ghouiri@outlook.fr>
2024-09-13 16:51:04 +0200

    add accumulator getters and scenarioParser

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/Accumulator.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/Accumulator.kt
@@ -8,7 +8,7 @@ class Accumulator {
     private var ships : MutableMap<Int, Ship> = mutableMapOf()
     private var events: MutableMap<Int, Event> = mutableMapOf()
     private var garbage: MutableMap<Int, Garbage> = mutableMapOf()
-    private var Tasks: MutableMap<Int, Task> = mutableMapOf()
+    private var tasks: MutableMap<Int, Task> = mutableMapOf()
     private var rewards: MutableMap<Int, Reward> = mutableMapOf()
     private var mapCorporationToHarbor: Map<Int, MutableList<Tile>> = mutableMapOf()
     private var mapCorporationToShips: Map<Int, MutableList<Int>> = mutableMapOf()
@@ -16,7 +16,7 @@ class Accumulator {
     public fun getHarborsOfCorporation(corporationId: Int): List<Tile>? {
         return mapCorporationToHarbor[corporationId]
     }
-    public fun getShipsbyId(shipId: Int): Ship? {
+    public fun getShipsById(shipId: Int): Ship? {
         return ships[shipId]
     }
     public fun getTileById(tileId: Int): Tile? {
@@ -24,6 +24,9 @@ class Accumulator {
     }
     public fun getRewardById(rewardId: Int): Reward? {
         return rewards[rewardId]
+    }
+    public fun addReward(rewardId: Int, reward: Reward){
+        rewards[rewardId] = reward
     }
     public fun addTask(taskId: Int, task: Task): Unit {
         tasks[taskId] = task
@@ -37,7 +40,9 @@ class Accumulator {
     public fun addTile(id: Int, t: Tile): Unit {
 
     }
-    public fun addTileByCoordinates(coord: Vec2D, t: Tile): Unit {}
+    public fun addTileByCoordinates(coord: Vec2D, t: Tile): Unit {
+
+    }
     public fun addShip(shipId: Int, ship: Ship) {
         ships[shipId] = ship
     }
@@ -48,7 +53,10 @@ class Accumulator {
         return mapCorporationToShips[corpId]
     }
     public fun addEvent(eventId: Int, event: Event) {
-
+        events[eventId] = event
+    }
+    public fun getEventById(eventId: Int): Event? {
+        return events[eventId]
     }
     public fun addCorporation(corpId: Int, corporation: Corporation) {
         corporations[corpId] = corporation
--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/ScenarioJSONParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/ScenarioJSONParser.kt
@@ -2,7 +2,7 @@ package de.unisaarland.cs.se.selab.parser
 
 import org.json.JSONObject
 
-class ScenarioJSONParser(accumulator: Accumulator) {
+class ScenarioJSONParser(override val accumulator: Accumulator): JSONParser {
     public fun parseEvent(EventJSON: String): Boolean {
 
     }
@@ -24,27 +24,60 @@ class ScenarioJSONParser(accumulator: Accumulator) {
             validateGarbage(garbage)
         }
     }
-    private fun validateGarbage(garbage: JSONObject): Boolean{}
+    private fun validateGarbage(garbage: JSONObject): Boolean{
+        val uniqueId: Boolean=  accumulator.getGarbageById(garbage.getInt("id")) == null
+        val locationExists: Boolean= accumulator.getTileById(garbage.getInt("location")) != null
+        if (uniqueId && locationExists){
+            val garbageObj: Garbage = createGarbage(garbage)
+            accumulator.addGarbage(garbageObj.getId(), garbageObj)
+            return true
+        }
+        return false
+    }
     private fun validateEvents(events:List<JSONObject>): Boolean{
         for (event in events){
             validateEvent(event)
         }
     }
     private fun validateEvent(event: JSONObject): Boolean{
-
+        val uniqueId: Boolean = accumulator.getEventById(event.getInt("id")) == null
+        //val event : Boolean = accumulator.getShipsbyId(event.getInt("shipID")) != null
+        // TODO: Conditions to add
+        if (uniqueId){
+            val eventObj: Event = createEvent(event)
+            accumulator.addEvent(event.getInt("id"), eventObj)
+            return true
+        }
+        return false
     }
     private fun validateTasks(tasks: List<JSONObject>): Boolean{
         for (task in tasks){validateTask(task)}
     }
     private fun validateTask(task: JSONObject): Boolean{
-
+        val uniqueId: Boolean = accumulator.getTaskById(task.getInt("id")) == null
+        if (uniqueId){
+            val taskObj : Task = createTask(task)
+            accumulator.addTask(taskObj.getId(), taskObj)
+            return true
+        }
+        return false
     }
     private fun validateRewards(rewards: List<JSONObject>): Boolean{
         for (reward in rewards){validateReward(reward)}
     }
-    private fun validateReward(reward: JSONObject): Boolean{}
+    private fun validateReward(reward: JSONObject): Boolean{
+        val uniqueId: Boolean = accumulator.getRewardById(reward.getInt("id")) == null
+        // TODO: Validation condition
+        if (uniqueId){
+            val r : Reward = createReward(reward)
+            accumulator.addReward(r.getId(), r)
+            return true
+        }
+        return false
+    }
     private fun createGarbage(garbage: JSONObject): Garbage{}
     private fun createEvent(event: JSONObject): Event{}
     private fun createReward(reward: JSONObject): Reward{}
-    private fun  createTask(task: JSONObject): Task{}
+    private fun createTask(task: JSONObject): Task{}
+
 }
