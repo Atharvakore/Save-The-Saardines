38643235ac19e8322fecc8288b2e171396fa211f
Fedayn <badr.ghouiri@outlook.fr>
2024-09-13 19:54:33 +0200

    parser for garbage and events

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/ScenarioJSONParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/ScenarioJSONParser.kt
@@ -1,44 +1,30 @@
 package de.unisaarland.cs.se.selab.parser
 
+import kotlinx.serialization.json.JsonElement
+import org.json.JSONArray
 import org.json.JSONObject
+import java.io.File
 
 class ScenarioJSONParser(override val accumulator: Accumulator): JSONParser {
-    public fun parseEvent(EventJSON: String): Boolean {
-
-    }
-
-
-
-    public fun parseEvents(EventJSON: String): Boolean{
-
-    }
-    public fun parseGarbage(garbageJSON: String): Boolean{
-
-    }
-    public fun parseTasks(taskJSON: String): Boolean{}
-    public fun parseRewards(rewardJSON: String): Boolean{
-
-    }
-    private fun validateGarbages(garbages:List<JSONObject>): Boolean{
-        for (garbage in garbages){
-            validateGarbage(garbage)
+    public fun parseEvents(eventsFile: String): Boolean{
+        try {
+            val events = JSONObject(File(eventsFile).readText()).getJSONArray("events")
+            return createEvents(events)
         }
+        catch(error: Exception){
+            return False
+        }
+
     }
-    private fun validateGarbage(garbage: JSONObject): Boolean{
-        val uniqueId: Boolean=  accumulator.getGarbageById(garbage.getInt("id")) == null
-        val locationExists: Boolean= accumulator.getTileById(garbage.getInt("location")) != null
-        if (uniqueId && locationExists){
-            val garbageObj: Garbage = createGarbage(garbage)
-            accumulator.addGarbage(garbageObj.getId(), garbageObj)
-            return true
+    public fun createEvents(events: JSONArray): Boolean{
+        for (event in events){
+            if (validateEvent(event as JSONObject)){
+                return createEvent(event)
+            }
         }
         return false
     }
-    private fun validateEvents(events:List<JSONObject>): Boolean{
-        for (event in events){
-            validateEvent(event)
-        }
-    }
+
     private fun validateEvent(event: JSONObject): Boolean{
         val uniqueId: Boolean = accumulator.getEventById(event.getInt("id")) == null
         //val event : Boolean = accumulator.getShipsbyId(event.getInt("shipID")) != null
@@ -46,10 +32,51 @@ class ScenarioJSONParser(override val accumulator: Accumulator): JSONParser {
         if (uniqueId){
             val eventObj: Event = createEvent(event)
             accumulator.addEvent(event.getInt("id"), eventObj)
+
             return true
         }
         return false
     }
+    public fun parseGarbage(garbageJSON: String): Boolean{
+        try {
+            val garbage = JSONObject(File(garbageJSON).readText()).getJSONArray("garbage")
+            return createGarbage(garbage)
+        }
+        catch(error: Exception){
+            return False
+        }
+
+    }
+    public fun createGarbage(garbage: JSONArray): Boolean{
+        for (gar in garbage){
+            if (validateGarbage(gar as JSONObject)){
+                return createEvent(gar)
+            }
+        }
+        return false
+    }
+
+    private fun validateGarbage(garbage: JSONObject): Boolean{
+        val uniqueId: Boolean=  accumulator.getGarbageById(garbage.getInt("id")) == null
+        val location: Tile? = accumulator.getTileById(garbage.getInt("location"))
+        val locationExists: Boolean=  location != null
+        val locationIsNotLand: Boolean = location !is Land
+        if (uniqueId && locationExists && locationIsNotLand){
+            val garbageObj: Garbage = createGarbage(garbage)
+            accumulator.addGarbage(garbageObj.getId(), garbageObj)
+            return true
+        }
+        return false
+    }
+
+
+    public fun parseTasks(taskJSON: String): Boolean{}
+    public fun parseRewards(rewardJSON: String): Boolean{
+
+    }
+
+
+
     private fun validateTasks(tasks: List<JSONObject>): Boolean{
         for (task in tasks){validateTask(task)}
     }
