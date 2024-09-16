83abcc23d348a97753675432f48ac96cf57df9ff
Aftab Khan <afkh00001@uni-saarland.de>
2024-09-16 16:10:59 +0200

    changes in sea object and added a test for sea

--- src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Sea.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Sea.kt
@@ -3,27 +3,29 @@ package de.unisaarland.cs.se.selab.tiles
 /**
  * Sea as static class
  */
-public object Sea {
-    val tiles: List<Tile> = listOf()
-    val tileIndex: Map<Vec2D, Tile> = mapOf()
+object Sea {
+    var tiles: List<Tile> = listOf()
+    var tileIndex: Map<Vec2D, Tile> = mapOf()
 
     /**
      * gives tile with the ID given as argument
      */
 
-    public fun getTileById(tileId: Int): Tile? {
+    fun getTileById(tileId: Int): Tile? {
         for (tile in tiles) {
             if (tile.id == tileId) return tile
         }
         return null
     }
+
     /**
      * gives tile with the position given as argument
      */
-    public fun getTileByPos(position: Vec2D): Tile? {
+    fun getTileByPos(position: Vec2D): Tile? {
         for (tile in tiles) {
             if (tile.pos == position) return tile
         }
         return null
     }
+
 }
