87dc1c0d8d11dc16483135069b6d51b059ce4530
Atharvakore <delandatharva@gmail.com>
2024-09-16 16:45:36 +0200

    drift logic advanced

--- src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Garbage.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Garbage.kt
@@ -39,7 +39,10 @@ class Garbage(
         var toBedrifted = localcurrent?.speed?.div(TEN)
             ?.let { localcurrent.direction?.let { it1 -> currentTile.getTileInDirection(it, it1) } }
         for (g in currentTile.garbage.sortedBy { it.id }) {
-            if(g.amount + currentTile.amountOfGarbageDriftedThisTick < ammounttobedrifted!!) {
+            if(g.amount >= ammounttobedrifted!!){
+                driftBigGarbages(ammounttobedrifted,g,currentTile,toBedrifted)
+            }
+            if (g.amount + currentTile.amountOfGarbageDriftedThisTick < ammounttobedrifted!!) {
                 if (g.type == GarbageType.OIL) {
                     driftOil(g, currentTile, toBedrifted)
                     ammounttobedrifted = ammounttobedrifted?.minus(g.amount)
@@ -50,6 +53,22 @@ class Garbage(
         }
 
 
+    }
+
+    /**
+     * drifts big garbage which have more amount than Intensity * 50
+     */
+
+    private fun driftBigGarbages (ammounttobedrifted:Int, g: Garbage, source: DeepOcean, target: Tile?){
+        if(g.type == GarbageType.OIL){
+            if (target!!.currentOilLevel() + g.amount <= THOUSAND) {
+               target.addGarbage( createGarbage(ammounttobedrifted, GarbageType.OIL))
+                source.garbage [source.garbage.indexOf(g)].amount-=ammounttobedrifted
+            }
+        }
+        target!!.addGarbage( createGarbage(ammounttobedrifted, g.type))
+        source.garbage [source.garbage.indexOf(g)].amount-=ammounttobedrifted
+        source.amountOfGarbageDriftedThisTick = ammounttobedrifted
     }
 
     /**
@@ -68,13 +87,13 @@ class Garbage(
     /**
      * drifts plastic and chemicals
      */
-    private fun driftPlasticandChemicals(g: Garbage, source: DeepOcean, target: Tile?){
+    private fun driftPlasticandChemicals(g: Garbage, source: DeepOcean, target: Tile?) {
         if (target != null) {
-            if (target.currentOilLevel() + g.amount <= THOUSAND) {
+
                 target.addGarbage(g)
                 source.garbage.filter { it == g }
                 source.amountOfGarbageDriftedThisTick += g.amount
-            }
+
         }
     }
 
