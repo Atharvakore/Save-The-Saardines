6da52af9370cffa2cf59163924c09684c73c24c5
Msaadad <mohmedsd0211@gmail.com>
2024-09-16 14:57:35 +0200

    fixed getTileWith....

--- src/main/kotlin/de/unisaarland/cs/se/selab/ships/ScoutingShip.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/ships/ScoutingShip.kt
@@ -1,29 +1,26 @@
 package de.unisaarland.cs.se.selab.ships
 
-import de.unisaarland.cs.se.selab.tiles.Direction
+import de.unisaarland.cs.se.selab.tiles.Sea
 import de.unisaarland.cs.se.selab.tiles.Tile
 
 
 class ScoutingShip (var visibilityRange: Int): ShipCapability {
-    private val directions = listOf(
-        Direction.D0, Direction.D60, Direction.D120,
-        Direction.D180, Direction.D240, Direction.D300
-    )
-
     /**
      * Call: from corporation run method to gather information
      * Logic: the ship checks all tiles in its Visibility range for garbage and returns them
      *
-     * @param  shipTile the tile the ship is currently on
+     * @param sea the whole map
+     * @param shipTile the tile the ship is currently on
      * @return the list of all the tiles within its visibility range
      */
-    fun getTilesWithGarbageInFoV(shipTile: Tile): List<Tile> {
-        val tilesInFov = directions.map { direction ->
-            shipTile.getTileInDirection(visibilityRange, direction)
+    fun getTilesWithGarbageInFoV(sea: Sea, shipTile: Tile): List<Tile> {
+        val visibleTiles = shipTile.pos.tilesInRadius(visibilityRange)
+        val tilesInFov = mutableListOf<Tile?>()
+        for (tiles in visibleTiles){
+            tilesInFov.add(sea.getTileByPos(tiles))
         }
         return filterTilesWithGarbage(tilesInFov)
     }
-
     private fun filterTilesWithGarbage(tiles: List<Tile?>): List<Tile> {
         return tiles.filterNotNull()
             .filter { it.garbage.isNotEmpty() }
