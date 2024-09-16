46f2e47266b8f5b5f140ef4fbd3bd4cd7cd1f689
Fedayn <badr.ghouiri@outlook.fr>
2024-09-16 13:47:24 +0200

    add attribute for Direction enum, easy access

--- src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Direction.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Direction.kt
@@ -4,6 +4,17 @@ package de.unisaarland.cs.se.selab.tiles
  * An enumeration of the six possible directions in which
  * a tile can have a neighbour.
  */
-enum class Direction {
-    D0, D60, D120, D180, D240, D300
+enum class Direction(private val dir: Int) {
+    D0(0),
+    D60(60),
+    D120(120),
+    D180(180),
+    D240(240),
+    D300(300);
+
+    companion object {
+        fun getDirection(dir: Int): Direction? {
+            return entries.find { it.dir == dir }
+        }
+    }
 }
