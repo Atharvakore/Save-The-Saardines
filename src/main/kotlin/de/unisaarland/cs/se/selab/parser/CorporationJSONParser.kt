7ce7341b3b4cc12611b44db3627e3b39ca4536bd
ghenadie tofan <ghto00001@stud.uni-saarland.de>
2024-09-16 17:52:16 +0300

    started corporation parser

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/CorporationJSONParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/CorporationJSONParser.kt
@@ -1,51 +1,70 @@
 package de.unisaarland.cs.se.selab.parser
-
 import de.unisaarland.cs.se.selab.corporation.Corporation
-import de.unisaarland.cs.se.selab.logger.Logger
+import de.unisaarland.cs.se.selab.ships.Ship
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.json.JSONArray
 import org.json.JSONObject
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 
-class CorporationJSONParser(
-    override val accumulator: Accumulator,
-) : JSONParser {
+/** Parser and Validator for extracting data from Map File **/
+class CorporationJSONParser(override val accumulator: Accumulator) : JSONParser {
+    private val logger = KotlinLogging.logger {}
 
     /** Parsing function **/
-    public fun parseCorporations(filePath: String): Boolean {
-        return false;
+    public fun parseCorporationsFile(filePath: String): Boolean {
+        val file: File
+        val corporations: JSONArray
+        val ships: JSONArray
+        val objects: JSONObject
+        try {
+            file = File(filePath)
+        } catch (err: FileNotFoundException) {
+            logger.error { "file '$filePath' does not exist." }
+            return false
+        }
+        try {
+            objects = JSONObject(file.readText())
+            if (!objects.has(CORPORATIONS) || !objects.has(SHIPS)) {
+                return false
+            }
+            corporations = objects.getJSONObject(CORPORATIONS) as JSONArray
+            ships = objects.getJSONObject(SHIPS) as JSONArray
+        } catch (err: IOException) {
+            logger.error { "Failed to parse file '$filePath'" }
+            return false
+        }
+        val result = validateShips(ships) && validateCorporations(corporations)
+        return result
     }
 
-    public fun parseShips(filePath: String): Boolean {
-        return false;
-    }
-
-    private fun createCorporationObjects(filePath: String): List<JSONObject> {
-        //TODO
-    }
-
-    private fun validateCorporations(corpObjects: List<JSONObject>): Boolean {
-        return false;
+    private fun validateCorporations(corpObjects: JSONArray): Boolean {
+        return false
     }
 
     private fun validateCorporation(corporation: JSONObject): Boolean {
-        return false;
-    }
-
-    private fun createShipObjects(filePath: String): List<JSONObject> {
-
+        return false
     }
 
     private fun createShip(ship: JSONObject): Ship {
-
+        return  Ship()
     }
 
     private fun createCorporation(corporation: JSONObject): Corporation {
-
+        return Corporation()
     }
 
     private fun validateShip(shipObject: JSONObject): Boolean {
-        return false;
+        return false
     }
 
-    private fun validateShips(corporationObjects: List<JSONObject>): Boolean {
-        return false;
+    private fun validateShips(corporationObjects: JSONArray): Boolean {
+        return false
+    }
+
+    companion object Companion {
+        const val CORPORATIONS = "corporations"
+        const val SHIPS = "ships"
     }
 }
