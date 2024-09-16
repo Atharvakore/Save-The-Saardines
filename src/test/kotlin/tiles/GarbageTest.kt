a53c38d434cb5953312fb820b476cc1ecf8f29a6
Aftab Khan <afkh00001@uni-saarland.de>
2024-09-16 16:54:48 +0200

    basic testing framework

--- src/test/kotlin/tiles/GarbageTest.kt
+++ src/test/kotlin/tiles/GarbageTest.kt
@@ -1,4 +1,36 @@
 package tiles
 
+import kotlin.test.Test
+
 class GarbageTest {
+
+    @Test
+    fun testGarbage() {
+        TODO("Yet to be implemented")
+    }
+
+    @Test
+    fun testCreateGarbage() {
+        TODO("Yet to be implemented")
+    }
+
+    @Test
+    fun testDrift() {
+        TODO("Yet to be implemented")
+    }
+
+    @Test
+    fun testDriftBigGarbages() {
+        TODO("Yet to be implemented")
+    }
+
+    @Test
+    fun testDriftOil() {
+        TODO("Yet to be implemented")
+    }
+
+    @Test
+    fun testDriftPlasticAndChemicals() {
+        TODO("Yet to be implemented")
+    }
 }
