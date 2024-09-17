acfb9f58a53b2d47ddc5ed956738abbc7dc0a6cc
Aftab Khan <afkh00001@uni-saarland.de>
2024-09-17 11:40:52 +0200

    added test template for tile class

--- src/test/kotlin/tiles/TileTest.kt
+++ src/test/kotlin/tiles/TileTest.kt
@@ -1,5 +1,38 @@
 package tiles
 
+import de.unisaarland.cs.se.selab.tiles.Tile
+import kotlin.test.Test
+
 
 class TileTest {
+
+    @Test
+    fun testGetTileInDirection() {
+
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun testAddGarbage() {
+
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun testGetAmountOfType() {
+
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun testRemoveAmountOfType() {
+
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun testCurrentOilLevel() {
+
+        TODO("Not yet implemented")
+    }
 }
