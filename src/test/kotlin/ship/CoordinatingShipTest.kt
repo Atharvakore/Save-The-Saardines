7f0331c5f461c506290142987ce4f24e8fb4ee56
Fedayn <badr.ghouiri@outlook.fr>
2024-09-18 11:19:05 +0200

    restore ship tests

---
+++ src/test/kotlin/ship/CoordinatingShipTest.kt
@@ -0,0 +1,21 @@
+package ship
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CoordinatingShipTest {
+
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    /** Testing basic functionalities **/
+
+    @Test
+    fun testGetTilesWithGarbageInFoV() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/ship/ScoutingShipTest.kt
@@ -0,0 +1,21 @@
+package ship
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ScoutingShipTest {
+
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    /** Testing basic functionalities **/
+
+    @Test
+    fun testGetTilesWithGarbageInFoV() {
+        TODO("Not yet implemented")
+    }
+}
