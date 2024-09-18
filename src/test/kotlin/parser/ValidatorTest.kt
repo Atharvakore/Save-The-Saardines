3cbb5ea05c25525bc546f9536d2a95f0befd2be7
Fedayn <badr.ghouiri@outlook.fr>
2024-09-18 14:00:03 +0200

    validating test for given files passing

--- src/test/kotlin/parser/ValidatorTest.kt
+++ src/test/kotlin/parser/ValidatorTest.kt
@@ -12,10 +12,17 @@ class ValidatorTest {
         val scenarioFile: String = "src/systemtest/resources/scenarioJsons/scenario.json"
         val files: List<String> = mutableListOf(mapFile, corporationsFile, scenarioFile)
         val acc: Accumulator? = parse(files, 0, "stdout")
-        assert(acc != null)
-        /*if (acc != null) {
+        //println(acc!!.tiles)
+        if (acc != null) {
             assert(acc.tiles.size == 36)
+            print(acc.corporations.size)
             assert(acc.corporations.size == 2)
-            assert(acc.ships.size == 2)*/
+            assert(acc.ships.size == 2)
+            assert(acc.garbage.size == 1)
+            assert(acc.events.size == 1)
+            assert(acc.tasks.isEmpty())
+            assert(acc.rewards.isEmpty())
+
+        }
     }
 }
