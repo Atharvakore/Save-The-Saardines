23727daff054afb03ed2be51ca3b36486f6ff267
Badr Ghouiri <bagh00001@stud.uni-saarland.de>
2024-09-19 18:34:00 +0200

    add output parsing

--- src/test/kotlin/parser/ValidatorTest.kt
+++ src/test/kotlin/parser/ValidatorTest.kt
@@ -1,7 +1,10 @@
 package parser
 
+import de.unisaarland.cs.se.selab.logger.Logger
 import de.unisaarland.cs.se.selab.parse
 import de.unisaarland.cs.se.selab.parser.Accumulator
+import java.io.File
+import java.io.PrintWriter
 import kotlin.test.Test
 
 class ValidatorTest {
@@ -11,16 +14,19 @@ class ValidatorTest {
         val corporationsFile: String = "src/systemtest/resources/corporationJsons/corporations.json"
         val scenarioFile: String = "src/systemtest/resources/scenarioJsons/scenario.json"
         val files: List<String> = mutableListOf(mapFile, corporationsFile, scenarioFile)
+        val x = PrintWriter(File("outTest"))
+        Logger.setOutBuffer(x)
         val acc: Accumulator? = parse(files, 0, "stdout")
         if (acc != null) {
             assert(acc.tiles.size == 36)
             assert(acc.corporations.size == 2)
             assert(acc.ships.size == 2)
             assert(acc.garbage.size == 1)
-            assert(acc.events.size == 1)
+            assert(acc.events.size == 2)
             assert(acc.tasks.isEmpty())
             assert(acc.rewards.isEmpty())
         }
+        x.close()
     }
 
     @Test
