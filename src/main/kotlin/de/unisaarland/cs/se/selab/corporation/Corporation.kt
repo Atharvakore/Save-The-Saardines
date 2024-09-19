c70a3841cc781ddb25fae4b1b5e8b7990e995c61
Aftab Khan <afkh00001@uni-saarland.de>
2024-09-19 14:30:54 +0200

    log during cooperation between ships

--- src/main/kotlin/de/unisaarland/cs/se/selab/corporation/Corporation.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/corporation/Corporation.kt
@@ -53,14 +53,14 @@ class Corporation(
                 otherShips.filter { otherCorporations.contains(it.owner) }
 
             otherShipsToCooperate.forEach { otherShipToCooperate ->
-                getInfoFromShip(otherShipToCooperate)
+                getInfoFromShip(otherShipToCooperate, coordinatingShip)
             }
             val lastCorporation: Corporation? = otherCorporations.maxByOrNull { it.id }
             lastCoordinatingCorporation = lastCorporation
         }
     }
 
-    private fun getInfoFromShip(otherShip: Ship) {
+    private fun getInfoFromShip(otherShip: Ship, coordinatingShip: Ship) {
         val telescopes: List<ScoutingShip> = otherShip.capabilities
             .filterIsInstance<ScoutingShip>()
         telescopes.forEach { telescope ->
@@ -74,6 +74,12 @@ class Corporation(
                     .forEach { garbage -> partnerGarbage[garbage.id] = tile }
             }
         }
+        LoggerCorporationAction.logCooperationBetweenCorporations(
+            id,
+            otherShip.owner.id,
+            coordinatingShip.id,
+            otherShip.id
+        )
     }
 
     /**
