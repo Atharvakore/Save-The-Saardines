d06664b58a5c5be5218aa6d4f8c5a0684e7395b5
Aftab Khan <afkh00001@uni-saarland.de>
2024-09-20 12:50:49 +0200

    changes in vec2d test

--- src/test/kotlin/tiles/Vec2DTest.kt
+++ src/test/kotlin/tiles/Vec2DTest.kt
@@ -9,19 +9,22 @@ class Vec2DTest {
 
     @Test
     fun testGetTilesInRadius() {
-        val currPosition: Vec2D = Vec2D(1, 6)
+        val currPosition: Vec2D = Vec2D(2, 7)
         val radius: Int = 1
         val tilesInRadius: Iterator<Vec2D> = currPosition.tilesInRadius(radius)
         val expectedTilesInRadius: List<Vec2D> = listOf(
-            Vec2D(1, 5),
-            Vec2D(2, 5),
-            Vec2D(2, 7),
             Vec2D(2, 6),
+            Vec2D(1, 6),
             Vec2D(1, 7),
-            Vec2D(0, 6)
+            Vec2D(1, 8),
+            Vec2D(2, 8),
+            Vec2D(3, 7)
         )
 
-        val actualTilesInRadius: List<Vec2D> = tilesInRadius.asSequence().toList()
+        val actualTilesInRadius: MutableList<Vec2D> = mutableListOf()
+        tilesInRadius.forEach {
+            actualTilesInRadius.add(it)
+        }
 
         assert(
             expectedTilesInRadius.containsAll(actualTilesInRadius) &&
