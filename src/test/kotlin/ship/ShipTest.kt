d013d8a6ae47799447294dbf993893c888bc3c46
Aftab Khan <afkh00001@uni-saarland.de>
2024-09-20 14:27:34 +0200

    fixed ship drift and test

--- src/test/kotlin/ship/ShipTest.kt
+++ src/test/kotlin/ship/ShipTest.kt
@@ -8,6 +8,7 @@ import de.unisaarland.cs.se.selab.tiles.Direction
 import de.unisaarland.cs.se.selab.tiles.ShallowOcean
 import de.unisaarland.cs.se.selab.tiles.Tile
 import de.unisaarland.cs.se.selab.tiles.Vec2D
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -17,11 +18,13 @@ import org.junit.jupiter.api.TestInstance
 class ShipTest {
 
     private val ship: Ship = Ship(1, 10, 5, 3000, 7, mutableListOf())
-    private val tile37: Tile = DeepOcean(37, Vec2D(6, 3), emptyList(), emptyList(), Current(10, Direction.D0, 1))
-    private val tile38: Tile = ShallowOcean(38, Vec2D(7, 3), emptyList(), emptyList())
+    private lateinit var tile37: Tile
+    private lateinit var tile38: Tile
 
     @BeforeEach
     fun setUp() {
+        tile37 = DeepOcean(37, Vec2D(6, 3), emptyList(), emptyList(), Current(10, Direction.D0, 1))
+        tile38 = ShallowOcean(38, Vec2D(7, 3), emptyList(), emptyList())
         tile37.adjacentTiles = listOf(tile38)
         tile38.adjacentTiles = listOf(tile37)
     }
