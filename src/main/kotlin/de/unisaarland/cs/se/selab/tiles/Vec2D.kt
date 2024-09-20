e02d34025b2ad7a0bda58a1c8698db392ed26c54
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-09-20 15:26:59 +0200

    eh

--- src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Vec2D.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/tiles/Vec2D.kt
@@ -1,5 +1,7 @@
 package de.unisaarland.cs.se.selab.tiles
 
+import kotlin.math.abs
+
 /**
  * A simple 2D vector class. It is used to represent positions on the sea.
  * @property posX the x-coordinate of the vector
@@ -28,10 +30,21 @@ class Vec2D(var posX: Int, var posY: Int) {
      * Returns an iterator over *all* the Vec2D instances in a given radius around this Vec2D.
      */
     fun tilesInRadius(radius: Int) = iterator {
-        // axial coordinate magic i found online.
-        for (dq in -radius..radius) {
-            for (dr in (-radius).coerceAtLeast(-dq - radius)..radius.coerceAtMost(-dq + radius)) {
-                yield(Vec2D(posX + dq, posY + dr))
+        val centerX = posX - (posY + (posY and 1)) / 2
+        val centerZ = posY
+        val centerY = -centerX - centerZ
+        for (dx in -radius..radius) {
+            for (dy in -radius..radius) {
+                for (dz in -radius..radius) {
+                    if (dx + dy + dz == 0) {
+                        val x = centerX + dx
+                        val z = centerZ + dz
+                        val y = centerY + dy
+                        val col = x + (z + (z and 1)) / 2
+                        val row = z
+                        yield(Vec2D(col, row))
+                    }
+                }
             }
         }
     }
