f71987d0bcf2eef1c9c8eb177a4e999203a6620d
Atharvakore <delandatharva@gmail.com>
2024-09-23 16:27:55 +0200

    Storm Event System Tested and and unit test added

---
+++ src/test/kotlin/events/StormTest.kt
@@ -0,0 +1,41 @@
+package test.kotlin.events
+
+import corporation.UTFactory
+import de.unisaarland.cs.se.selab.events.Storm
+import de.unisaarland.cs.se.selab.tiles.Direction
+import de.unisaarland.cs.se.selab.tiles.Garbage
+import de.unisaarland.cs.se.selab.tiles.GarbageType
+import de.unisaarland.cs.se.selab.tiles.Sea
+import de.unisaarland.cs.se.selab.tiles.Vec2D
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class StormTest {
+    var factory = UTFactory()
+    lateinit var sea: Sea
+
+    @BeforeEach
+    fun setUp() {
+        factory.createTestingMap()
+        sea = factory.sea
+        sea.getTileByPos(Vec2D(6, 6))?.addGarbage(
+            Garbage(1, 1000, GarbageType.OIL, null)
+        )
+    }
+
+    @Test
+    fun testStorm() {
+        val storm = Storm(
+            1,
+            0,
+            sea,
+            sea.getTileByPos(Vec2D(6, 6))!!,
+            0,
+            30
+        )
+        storm.direction = Direction.D180
+        assertTrue(storm.actUponTick(0))
+        assertTrue(sea.getTileByPos(Vec2D(3, 6))!!.currentOilLevel() == 1000)
+    }
+}
