9b3496350959f09d3fbd48e9a758e31efeadbcd7
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-09-23 14:17:34 +0200

    none of this is nullable?

--- src/main/kotlin/de/unisaarland/cs/se/selab/events/Storm.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/events/Storm.kt
@@ -8,13 +8,13 @@ import de.unisaarland.cs.se.selab.tiles.Tile
 class Storm(
     override val id: Int,
     override val fireTick: Int,
-    override val map: Sea?,
-    override val location: Tile?,
+    override val map: Sea,
+    override val location: Tile,
     override val radius: Int,
     val speed: Int,
 ) : LocalEvent(id, fireTick, map, location, radius) {
     lateinit var direction: Direction
-    val sea = Sea()
+
     override fun toString(): String {
         return "STORM"
     }
@@ -22,14 +22,10 @@ class Storm(
     override fun actUponTick(currentTick: Int): Boolean {
         val result: Boolean
         if (currentTick == fireTick) {
-            val radiusVec2D = location?.pos?.tilesInRadius(radius)
+            val radiusVec2D = location.pos.tilesInRadius(radius)
             val tiles: MutableList<Tile> = mutableListOf()
-
-            if (radiusVec2D == null) {
-                return false
-            }
             for (pos in radiusVec2D) {
-                sea.getTileByPos(pos)?.let { tiles.add(it) }
+                map.getTileByPos(pos)?.let { tiles.add(it) }
             }
             val tilesWithGarbage = tiles.filter { it.garbage.isNotEmpty() }
             for (tile in tilesWithGarbage) {
