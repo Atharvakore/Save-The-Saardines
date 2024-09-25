6a843676bddd42b9e88f56dcfa570ffd602bcc94
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-09-25 12:52:22 +0200

    aaaaaaaaaaaaa

--- src/test/kotlin/events/OilSpillTest.kt
+++ src/test/kotlin/events/OilSpillTest.kt
@@ -7,7 +7,6 @@ import de.unisaarland.cs.se.selab.tiles.Garbage
 import de.unisaarland.cs.se.selab.tiles.GarbageType
 import de.unisaarland.cs.se.selab.tiles.Sea
 import de.unisaarland.cs.se.selab.tiles.Vec2D
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
