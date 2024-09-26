bbdc760845c948748759ac70625e82d33c3f3a50
Msaadad <mosa00004@stud.uni-saarland.de>
2024-09-26 08:11:17 +0200

    fix

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/simulationtests/tasks/EventSchedulingTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/simulationtests/tasks/EventSchedulingTest.kt
@@ -10,26 +10,22 @@ class EventSchedulingTest : ExampleSystemTestExtension() {
     override val corporations = "tasksAndRewardsJsons/taskCorporation.Json"
     override val scenario = "tasksAndRewardsJsons/taskScenario.Json"
     override val map = "mapFiles/map_medium_01.json"
-    override val name = "ExploreMapTaskTest"
+    override val name = "EventSchedulingTest"
     override val maxTicks = 6
 
     override suspend fun run() {
-        skipUntilString("Current Drift: Ship 3 drifted from tile 65 to tile 66.")
-        assertNextLine("Event: Event 15 of type RESTRICTION happened.")
+        skipUntilString("Event: Event 15 of type RESTRICTION happened.")
 
-        skipUntilString("Current Drift: Ship 1 drifted from tile 66 to tile 67.")
-        assertNextLine("Event: Event 16 of type RESTRICTION happened.")
+        skipUntilString("Event: Event 16 of type RESTRICTION happened.")
 
-        skipUntilString("Current Drift: Ship 2 drifted from tile 66 to tile 67.")
-        assertNextLine("Event: Event 17 of type RESTRICTION happened.")
+        skipUntilString("Event: Event 17 of type RESTRICTION happened.")
 
-        skipUntilString("Current Drift: Ship 2 drifted from tile 67 to tile 68.")
-        assertNextLine("Event: Event 18 of type RESTRICTION happened.")
+        skipUntilString("Event: Event 18 of type RESTRICTION happened.")
 
-        skipUntilString("Current Drift: Ship 3 drifted from tile 66 to tile 67.")
-        assertNextLine("Event: Event 19 of type RESTRICTION happened.")
+        skipUntilString("Event: Event 19 of type RESTRICTION happened.")
 
-        skipUntilString("Current Drift: Ship 5 drifted from tile 66 to tile 67.")
-        assertNextLine("Event: Event 20 of type RESTRICTION happened.")
+        skipUntilString("Event: Event 20 of type RESTRICTION happened.")
+
+        assertNextLine("Simulation Info: Simulation ended.")
     }
 }
