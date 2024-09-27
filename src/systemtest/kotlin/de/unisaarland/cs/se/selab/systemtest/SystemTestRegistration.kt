c58ae8344fbcb18ea6fa724275d666b69a171079
Msaadad <mosa00004@stud.uni-saarland.de>
2024-09-27 12:23:18 +0200

    free gift test

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -5,7 +5,13 @@ import de.unisaarland.cs.se.selab.systemtest.basictests.sequencetick25.SequenceT
 import de.unisaarland.cs.se.selab.systemtest.basictests.sequencetick25.SequenceTick25StatisticsTest
 import de.unisaarland.cs.se.selab.systemtest.basictests.sequencetick25.SequenceTick25TickTest
 import de.unisaarland.cs.se.selab.systemtest.runner.SystemTestManager
-import de.unisaarland.cs.se.selab.systemtest.simulationtests.*
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.CollectAndRefuelTest
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.DriftGarbageMultipleCurrents
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.DriftGarbageOnLandTest
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.DriftShipsTest
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.SimulatePirateAttack
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.SimulateStormTest
+import de.unisaarland.cs.se.selab.systemtest.simulationtests.StormOverMultipleTiles
 import de.unisaarland.cs.se.selab.systemtest.simulationtests.collectingGarbage.AvengersAssemble
 import de.unisaarland.cs.se.selab.systemtest.simulationtests.collectingGarbage.CollectingChemicalsTest
 import de.unisaarland.cs.se.selab.systemtest.simulationtests.collectingGarbage.CollectingGarbageTest
@@ -130,8 +136,8 @@ object SystemTestRegistration {
         AdventureTime(),
         TaskInRestrictTest(),
         Error404Test(),
-        Blyat()
-        MultiTaskingTest()
+        MultiTaskingTest(),
+        FindGarbageTaskTest()
     )
 
     private val testsForMutants = listOf(
@@ -184,7 +190,6 @@ object SystemTestRegistration {
         AdventureTime(),
         AvengersAssemble(),
         OilBillionaire(),
-        Blyat()
     )
 
     /**
